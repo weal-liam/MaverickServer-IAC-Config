5c0065bd410041c317ce4528463d67d849675295
unknown <gw95845@gmail.com>
2025-06-02 11:25:13 +0400

    u

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,6 +94,13 @@ resource "aws_security_group" "weal_sg" {
   }
 }
 
+
+resource "aws_key_pair" "maverick_key" {
+  key_name   = "maverick_key"
+  public_key = file("~/.ssh/maverick_key.pub")
+}
+
+
 resource "aws_instance" "maverick_server" {
   ami           = data.aws_ami.maverick_server_ami.id
   instance_type = "t3.micro"
