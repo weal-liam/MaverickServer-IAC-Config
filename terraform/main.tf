343512f1fa9e2bf3193cd08c2c57150f79e3b929
Weal_liam <gw95845@gmail.com>
2025-12-17 00:28:45 +0300

    new steps

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,6 +92,15 @@ resource "aws_security_group" "weal_sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow SSH access"
   }
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
